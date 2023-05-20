e60f27659a09d91a305adf6a5c8f2bf544219796
Nich4da <124499918+Nich4da@users.noreply.github.com>
2023-05-20 18:59:30 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of segments and the range (N K):");
+        string[] input = Console.ReadLine().Split(' ');
+        int n = int.Parse(input[0]);
+        int k = int.Parse(input[1]);
+
+        Console.WriteLine("Enter the population for each segment (separated by a space):");
+        string[] populationInput = Console.ReadLine().Split(' ');
+        int[] population = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            population[i] = int.Parse(populationInput[i]);
+        }
+
+        int maxCustomers = GetMaxCustomers(population, k);
+        Console.WriteLine("Maximum number of customers: " + maxCustomers);
+    }
+
+    static int GetMaxCustomers(int[] population, int k)
+    {
+        int maxCustomers = 0;
+
+        for (int i = 0; i < population.Length; i++)
+        {
+            int currentSum = 0;
+
+            for (int j = i; j <= i + k && j < population.Length; j++)
+            {
+                currentSum += population[j];
+                maxCustomers = Math.Max(maxCustomers, currentSum);
+            }
+        }
+
+        return maxCustomers;
+    }
+}
